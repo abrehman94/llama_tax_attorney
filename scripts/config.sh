0f1fd47f1aa1809490a97e25ab0533271099e688
Abdul Rehman <abdulrehmanee010@gmail.com>
2023-11-22 12:39:21 -0500

    .

---
+++ scripts/config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+get_real_path() {
+    script_path=$(dirname $1)
+    script_path=$(realpath $script_path)
+}
+if [[ $0 != $BASH_SOURCE ]]; then
+    get_real_path $BASH_SOURCE
+else
+    get_real_path $0
+fi
+
+base=$script_path/../
+
