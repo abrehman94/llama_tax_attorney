e1aadaf822cef8cca4e3e21090de3a8b3a6c7153
Abdul Rehman <abdulrehmanee010@gmail.com>
2023-12-08 11:45:32 -0500

    scripts for use in llama repo

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+torchrun --nproc_per_node 1 \
+     example_chat_interaction.py \
+    --ckpt_dir ./models/7B/ \
+    --tokenizer_path ./models/tokenizer.model \
+    --max_seq_len 512 --max_batch_size 6
+
+    # example_chat_completion.py \
+    # example_chat_completion.py \
+    # --ckpt_dir llama-2-7b/ \
