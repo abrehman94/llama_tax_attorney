9ed1744ad4486168dccacb444c179e6bfa0b9e4b
Abdul Rehman <abdulrehmanee010@gmail.com>
2023-12-11 22:26:25 -0500

    .

--- accuracy_analysis/batch_processing.py
+++ accuracy_analysis/batch_processing.py
@@ -1,6 +1,3 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
-
 from typing import List, Optional
 import fire
 from llama import Llama, Dialog
@@ -10,26 +7,6 @@ import select
 import sys
 import csv
 import pickle
-
-def is_stdin_data_available():
-    readable, _, _ = select.select([sys.stdin], [], [], 0)
-    return bool(readable)
-
-def ignore_remaining_input():
-    try:
-        while is_stdin_data_available():
-            sys.stdin.readline()
-    except KeyboardInterrupt:
-        pass
-
-def wait_and_get_input():
-    try:
-        query = input("\n")
-        while is_stdin_data_available():
-            query += sys.stdin.readline()
-    except KeyboardInterrupt:
-        pass
-    return query
 
 def build_generator(    
                     ckpt_dir: str,
@@ -147,7 +124,7 @@ def main(
     
     generator = build_generator(ckpt_dir, tokenizer_path)
     
-    if False: 
+    if True: 
         bsize = 8
         query = []
         mans = []
@@ -191,63 +168,6 @@ def main(
         save_obj( base+'/model_ans.pkl', mans )
         save_obj( base+'/actual_ans.pkl',aans )
 
-def redundant(
-):
-    """
-    Entry point of the program for generating text using a pretrained model.
-
-    Args:
-        ckpt_dir (str): The directory containing checkpoint files for the pretrained model.
-        tokenizer_path (str): The path to the tokenizer model used for text encoding/decoding.
-        temperature (float, optional): The temperature value for controlling randomness in generation.
-            Defaults to 0.6.
-        top_p (float, optional): The top-p sampling parameter for controlling diversity in generation.
-            Defaults to 0.9.
-        max_seq_len (int, optional): The maximum sequence length for input prompts. Defaults to 512.
-        max_batch_size (int, optional): The maximum batch size for generating sequences. Defaults to 8.
-        max_gen_len (int, optional): The maximum length of generated sequences. If None, it will be
-            set to the model's max sequence length. Defaults to None.
-    """
-   
-    sys_dic = None 
-    while True:
-
-        ignore_remaining_input()
-        print("\n"+"=="*20+"\n")
-        query = wait_and_get_input() 
-        print("Query: {}".format(query))
-        
-        qs = query.split(':')
-        if qs[0].lower() == 'system':
-            query = "".join(qs[1:])
-            sys_dic = {"role": "system", "content": query}
-            print("Understood!")
-            continue
-        else:
-            if sys_dic is not None:
-                dialogs: List[Dialog] = [
-                    [sys_dic,
-                     {"role": "user", "content": query}],
-                ]
-            else:
-                dialogs: List[Dialog] = [
-                    [{"role": "user", "content": query}],
-                ]
-        results = generator.chat_completion(
-                dialogs,  # type: ignore
-                max_gen_len=max_gen_len,
-                temperature=temperature,
-                top_p=top_p,
-            )
-        for dialog, result in zip(dialogs, results):
-            # for msg in dialog:
-            #    print(f"{msg['role'].capitalize()}: {msg['content']}\n")
-            print(
-                f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}"
-            )
-
 if __name__ == "__main__":
     fire.Fire(main)
-
-
 
